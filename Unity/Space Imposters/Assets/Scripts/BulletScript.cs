42b7fdeb7ab855af49955525b285801fcdd30cb1
markkleeb <kleeb930@gmail.com>
2022-02-27 10:43:30 -0500

    Week 5

---
+++ Unity/Space Imposters/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    //bullet speed variable
+    public float speed = 1f;
+
+
+    private GameController gcscript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gcscript = GameObject.FindObjectOfType<GameController>();
+        Destroy(gameObject, 5);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up* speed* 0.1f, Space.World);
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Invader")
+        {
+            gcscript.AddScore();
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+
+            //Nothing will happen after this point
+
+        }
+    }
+
+}
---
+++ Unity/Space Imposters/Assets/Scripts/InvaderScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvaderScript : MonoBehaviour
+{
+    //invader speed variable
+    public static float speed = 2;
+    private GameController gcscript;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gcscript = GameObject.FindObjectOfType<GameController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move to the right every frame
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    //collision function
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //check to see if we collide with the wall
+        if (collision.gameObject.tag == "LeftWall")
+        {
+            speed = 2;
+
+            GameObject[] invaders = GameObject.FindGameObjectsWithTag("Invader");
+
+            foreach (GameObject i in invaders)
+            {
+                i.transform.Translate(Vector2.down * 20 * Time.deltaTime);
+            }
+        }
+
+        if (collision.gameObject.tag == "RightWall")
+        {
+            speed = -2;
+
+            GameObject[] invaders = GameObject.FindGameObjectsWithTag("Invader");
+
+            foreach (GameObject i in invaders)
+            {
+                i.transform.Translate(Vector2.down * 20 * Time.deltaTime);
+            }
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+
+            Destroy(collision.gameObject);
+            gcscript.killed();
+
+        }
+
+
+    }
+}
